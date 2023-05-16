962ff04d336537de8ce84ee48d041b4ce91ecd6e
Adrian <adrian.adam.zaleski@gmail.com>
2023-05-16 19:02:43 +0200

    rozdzielenie warstwy wykonawczej od interfejsu

--- audio_normaliser.py
+++ audio_normaliser.py
@@ -1,101 +1,82 @@
 import os
+import pprint
 from pydub import AudioSegment
-from pydub.utils import which
-from datetime import datetime
+from pydub.utils import which, mediainfo
 
-# wskazanie ścieżki do ffmpeg
+# wskazanie ścieżki do ffmpeg - biblioteki potrzebnej do konwersji audio
 ffmpeg_path = which("ffmpeg")
 
 
-# TODO:
-# - Zapisanie wartości normalizacji do pliku
-# - Opcja do zaczytania wartości z pliku do normalizacji
-# - Przerabianie plików wewnątrz podfolderów
-# - Dodanie wgrywania oryginalnych metadanych z oryginału do normalizowanego pliku (tytuł, artysta, numer ścieżki itp.)
-
-def normalize_volume(mp3_files, target_dBFS):
-    
+def normalize_volume(mp3_dir, mp3_files, target_dBFS):
     for file in mp3_files:
-        print('******************\n')
-        print(f'Przerabiam utwor: {file}')
+        print("******************\n")
+        print(f"Przerabiam utwor: {file}")
 
         # załadowanie pliku mp3
         song = AudioSegment.from_file(file, format="mp3", ffmpeg_path=ffmpeg_path)
-        
+
         # dodanie wartości głośności do listy przed normalizacją
-        print(f'Glosnosc przed: {song.dBFS}')
+        print(f"Glosnosc przed: {song.dBFS}")
 
         # normalizacja poziomu głośności
         normalized_song = song.apply_gain(target_dBFS - song.dBFS)
-       
+
         # zapisanie znormalizowanego pliku mp3
-        output_file = os.path.join(mp3_dir, 'normalized', f"normalized_{os.path.basename(file)}")
+        output_file = os.path.join(
+            mp3_dir, "normalized", f"normalized_{os.path.basename(file)}"
+        )
         normalized_song.export(output_file, format="mp3")
-        
+
         # dodanie wartości głośności do listy przed normalizacją
-        print(f'Glosnosc po normalizacji: {normalized_song.dBFS}')
-    
-    print('**********\n')
+        print(f"Glosnosc po normalizacji: {normalized_song.dBFS}")
+
+    print("**********\n")
     print("Zakończono normalizację głośności.")
 
 
-def avg_volume(mp3_files):
-    
+def avg_volume(mp3_files: list) -> int:
+    """Funkcja do określania średniej wartości głośności plików z zadanej listy.
+
+    Args:
+        mp3_files (list): lista plików, z której zostanie obliczona średnia wartość głośności
+
+    Returns:
+        int: średnia wartość głośności z listy
+    """
+
     # lista wartości głośności
     volumes = []
 
     for track in mp3_files:
         song = AudioSegment.from_file(track, format="mp3", ffmpeg_path=ffmpeg_path)
-        
+
         # dodanie wartości głośności do listy przed normalizacją
-        print(f'Glosnosc przed: {song.dBFS}')
+        print(f"Glosnosc przed: {song.dBFS} - utwor: {track}")
         volumes.append(song.dBFS)
+        print(f'\n*************************\n')
+        # Wywołanie metadanych:
+        meta_dane_z_pliku(track)
 
     # obliczenie minimalnej, maksymalnej i średniej wartości głośności po normalizacji
     min_volume = min(volumes)
     max_volume = max(volumes)
     avg_volume = sum(volumes) / len(volumes)
-
+    avg_volume = round(avg_volume, 2)
 
     print(f"Minimalna wartość głośności przed normalizacją:", min_volume)
     print(f"Maksymalna wartość głośności przed normalizacją:", max_volume)
-    print(f"Średnia wartość głośności dla {len(volumes)} przed normalizacją:", avg_volume)  
+    print(
+        f"Średnia wartość głośności dla {len(volumes)} przed normalizacją zaokrąglona do 2 miejsc po przecinku:",
+        avg_volume,
+    )
 
     return avg_volume
 
+def meta_dane_z_pliku(song):
+    
+    
+    # odczytanie metadanych z pliku oryginalnego
+    metadata = mediainfo(song)
+    pretty_metadata = pprint.pformat(metadata)
 
-
-# przykładowe użycie
-mp3_dir = input("Podaj ścieżkę do folderu z plikami mp3: ")
-
-
-# zapisanie czasu początkowego
-start_time = datetime.now()
-
-mp3_files = [os.path.join(mp3_dir, f) for f in os.listdir(mp3_dir) if f.lower().endswith('.mp3')]
-
-ilosc_utworow = len(mp3_files)
-# średnia wartość głośności zestawu utworów:
-avg_volume_value = avg_volume(mp3_files)
-
-print(f'\n###############################\n')
-
-normalize_volume(mp3_files, avg_volume_value)
-
-# zapisanie czasu końcowego
-end_time = datetime.now()
-
-# obliczenie czasu działania programu
-operation_time = end_time - start_time
-
-total_seconds = operation_time.total_seconds()
-
-# obliczenie godzin, minut i sekund
-hours, remainder = divmod(total_seconds, 3600)
-minutes, seconds = divmod(remainder, 60)
-
-# wyświetlenie czasu w formacie hh:mm:ss
-print(f"Czas działania programu dla {ilosc_utworow} utworów: {int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}")
-
-
-print(f'\n###############################\n')
+    print(f'metadata utworu: {song}: \n{pretty_metadata}')
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime
+
+from audio_normaliser import avg_volume, normalize_volume, meta_dane_z_pliku
+
+
+# TODO:
+# - Zapisanie wartości normalizacji do pliku
+# - Opcja do zaczytania wartości z pliku do normalizacji
+# - Przerabianie plików wewnątrz podfolderów
+# - Dodanie wgrywania oryginalnych metadanych z oryginału do normalizowanego pliku (tytuł, artysta, numer ścieżki itp.)
+
+
+# przykładowe użycie
+mp3_dir = input("Podaj ścieżkę do folderu z plikami mp3: ")
+
+
+# zapisanie czasu początkowego
+start_time = datetime.now()
+
+mp3_files = [
+    os.path.join(mp3_dir, f) for f in os.listdir(mp3_dir) if f.lower().endswith(".mp3")
+]
+
+ilosc_utworow = len(mp3_files)
+# średnia wartość głośności zestawu utworów:
+avg_volume_value = avg_volume(mp3_files)
+
+print(f"\n###############################\n")
+
+normalize_volume(mp3_dir, mp3_files, avg_volume_value)
+
+# zapisanie czasu końcowego
+end_time = datetime.now()
+
+# obliczenie czasu działania programu
+operation_time = end_time - start_time
+
+total_seconds = operation_time.total_seconds()
+
+# obliczenie godzin, minut i sekund
+hours, remainder = divmod(total_seconds, 3600)
+minutes, seconds = divmod(remainder, 60)
+
+# wyświetlenie czasu w formacie hh:mm:ss
+print(
+    f"Czas działania programu dla {ilosc_utworow} utworów: {int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+)
+
+
+print(f"\n###############################\n")
